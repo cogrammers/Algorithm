7f9869a69539ff98743d7927f6186943ab43b493
paulliu87 <paul.pliu87@gmail.com>
2018-01-21 15:02:23 -0800

    3 step rotation

---
+++ recoverRotatedSortedList/solution.java
@@ -0,0 +1,18 @@
+private void reverse(List<Integer> nums, int start, int end) {
+  for (int i = start, j = end; i < j; i++, j--) {
+    int temp = nums.get(i);
+    nums.set(i, nums.get(j));
+    nums.set(j, temp);
+  }
+}
+
+public void recoverRotatedSortedList(List<Integer> nums) {
+  for (int i = 0; i < nums.size() - 1; i++) {
+    if (nums.get(i) > nums.get(i + 1)) {
+      reverse(nums, 0, i);
+      reverse(nums, i + 1, nums.size() - 1);
+      reverse(nums, 0, nums.size() - 1);
+      return;
+    }
+  }
+}
