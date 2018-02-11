02c5a6878ff8a0a7e70021e91fbe69e97a0a1e41
Paul Liu <pliu@avvo.com>
2018-02-10 20:01:08 -0800

    add traverse solution

---
+++ preorder/preorderTraverse.java
@@ -0,0 +1,28 @@
+public class Solution {
+  public class TreeNode {
+    int value;
+    TreeNode left;
+    TreeNode right;
+    public TreeNode(int) {
+      this.value = int;
+      this.left = this.right = null;
+    }
+  }
+
+  public ArrayList<Integer> preorder(TreeNode root) {
+    ArrayList<Integer> result = new ArrayList<Integer>();
+    if (root == null) {
+      return result;
+    }
+    traverse(root, resul);
+    return result;
+  }
+  private void traverse(TreeNode root, ArrayList<Integer> result) {
+    if (root == null) {
+      return;
+    }
+    result.add(node.value);
+    traverse(root.left, result);
+    traverse(root.right, result);
+  }
+}
